cc19ebf2d60b0bcd97ff1d75403c2dc7f9194b98
= <dev.child.se@gmail.com>
2024-03-11 17:52:44 +0900

    apply quetion form logic

--- src/main/java/com/mysite/sbb/question/QuestionForm.java
+++ src/main/java/com/mysite/sbb/question/QuestionForm.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Setter
 public class QuestionForm {
 
-    @NotEmpty(message = "제폼은 필수 항목입니다.")
+    @NotEmpty(message = "제목은 필수 항목입니다.")
     @Size(max = 200)
     private String subject;
 
